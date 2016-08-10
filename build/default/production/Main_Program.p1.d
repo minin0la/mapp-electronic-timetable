c58d94c462a3fce1a92f47872cfefc548347f86a
Natawat Hebnak <minin0la17@gmail.com>
2016-08-10 10:23:49 +0800

    Add version number and on each LED when setting time for each subjects

--- build/default/production/Main_Program.p1.d
+++
@@ -1,6 +0,0 @@
- build/default/production/Main_Program.d  \
- build/default/production/Main_Program.p1:  \
- Main_Program.c  \
-lcd.h  \
-delays.h  \
-keypad.h 
--- build/default/production/seg7_utilities.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/seg7_utilities.d  \
- build/default/production/seg7_utilities.p1:  \
- seg7_utilities.c 
