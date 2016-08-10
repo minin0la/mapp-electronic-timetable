562282e79f00a18e535cdbb89a41c24b8218e1cb
Natawat Hebnak <minin0la17@gmail.com>
2016-08-10 12:26:52 +0800

    Moved the version numbers to version.c

---
+++ version.c
@@ -0,0 +1,24 @@
+/*
+ * File:   version.c
+ * Author: minin0la
+ *
+ * Created on 10 August, 2016, 12:15 PM
+ */
+
+
+#include <xc.h>
+#include "lcd.h"
+
+void version(void) //Program to show program version
+{
+	unsigned char msgindex, outchar;
+	char Version[ ] = "ETT Version 1.2     ";
+
+	lcd_write_cmd(0x80);
+
+	for (msgindex = 0; msgindex < 20; msgindex++)
+	{
+		outchar = Version[msgindex];
+		lcd_write_data(outchar);
+	}
+}
